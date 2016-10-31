5b31e22e2a494d97a0ba8118a743fec248938110
Daniel Popescu <dpopescu@adobe.com>
2016-10-31 16:31:21 +0200

    [DEV] Update Angular location strategy to use HashLocationStrategy

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 import {AppComponent} from './app.component';
 import {ExternalPageComponent} from './external-page/external-page.component';
@@ -40,7 +41,7 @@ import { SafePipePipe } from './safe-pipe.pipe';
       }
     ])
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule {

4874ae66c7f5f7951d91cbf7a2b13e1db23844cd
delete <pinheiro.llip@gmail.com>
2019-06-12 10:53:29 -0300

    Get back to webpack to build the lib

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+	entry: __dirname + '/ts-serializer',
+	mode: 'production',
+	devtool: 'inline-source-map',
+	resolve: {
+		extensions: [ '.tsx', '.ts', '.js' ]
+	  },
+	output:{
+		path: path.join(__dirname, 'dist'),
+		publicPath: '/',
+		filename: 'ts-serializer.js',
+		libraryTarget: 'umd',
+		library: 'TSerializer'
+	},
+	module: {
+		rules: [
+			{
+			  test: /\.tsx?$/,
+			  use: 'ts-loader',
+			  exclude: /node_modules/
+			}
+		  ]
+	}
+};
